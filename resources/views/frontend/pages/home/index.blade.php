0481a637c0c4bfa93d9d9c866698c70b96ef7f81
Faozul Azim Rafi <rafi_ccj@hotmail.com>
2023-12-20 19:51:47 +0600

    update medics section

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -471,8 +471,8 @@
     <!-- Section 4 - Why Choose Us -->
     <section id="section-medics" class="bg-secondary">
         <section class="container pt-5 pt-xl-2 pb-5 mb-2 mb-lg-3 mb-xl-4 mb-xxl-5">
-            <div class="row align-items-center pt-2 pt-sm-3 pt-md-4 pt-lg-5 mt-xl-2 mt-xxl-3">
-                <div class="col-md-6 col-xl-5 pb-3 pb-md-0 mb-4 mb-md-0">
+            <div class="row align-items-start pt-2 pt-sm-3 pt-md-4 pt-lg-5 mt-xl-2 mt-xxl-3">
+                <div class="col-md-6 col-xl-5 pb-3 pb-md-0 mb-4 mb-md-0 pt-lg-4">
 {{--                    <h2 class="display-6 mb-5 text-center text-white">Why Choose Us</h2>--}}
                     <div class="ratio ratio-1x1 d-flex align-items-center position-relative rounded-1 overflow-hidden bg-size-cover bg-position-bottom-center mx-auto" style="max-width: 530px; background-image: url('assets/images/frontend/home/medics-image.jpg');transform: scaleX(-1)">
                         <div class="position-absolute top-0 start-0 w-100 h-100"></div>
@@ -484,32 +484,105 @@
                         </div>-->
                     </div>
                 </div>
-                <div class="col-md-6 col-xl-5 offset-xl-1">
+                <div class="col-md-6 col-xl-6 offset-xl-1">
+                    <div class="ps-md-4 ps-lg-0">
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">M</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Motivation</h3>
+                                    <p class="card-text">
+                                        We create a positive and motivating environment to help our clients reach their full potential and exceed their own expectations.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back" data-aos-delay="250">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">E</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Excellence</h3>
+                                    <p class="card-text">
+                                        We strive for the highest level of quality and performance in all aspects of our work, constantly seeking to improve and set high standards for ourselves and others. We commit to a process of continuous growth and development.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back" data-aos-delay="500">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">D</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Dedication</h3>
+                                    <p class="card-text">
+                                        We are dedicated to providing a high-quality service and continuously improving our training and recruitment methods to meet the needs of our clients.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back" data-aos-delay="700">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">I</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Inclusivity</h3>
+                                    <p class="card-text">
+                                        We foster an inclusive culture that embraces the British values of fairness and tolerance of all in the workplace.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back" data-aos-delay="700">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">C</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Community</h3>
+                                    <p class="card-text">
+                                        We strive to build a strong community of medical professionals that can support each other in their professional development and career advancement.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="position-relative d-flex justify-content-end my-3 py-1" data-aos="fade-left" data-aos-easing="ease-out-back" data-aos-delay="700">
+                            <div class="btn btn-lg btn-icon btn-theme fs-xl fw-bold rounded-circle position-absolute top-50 start-0 translate-middle-y z-2 pe-none">S</div>
+                            <div class="card border-0 ps-3" style="width: calc(100% - 1.75rem);">
+                                <div class="card-body ps-4 py-4">
+                                    <h3 class="h5 card-title pb-2 mb-1">Support</h3>
+                                    <p class="card-text">
+                                        We offer unparalleled support to our clients, both during their training and as they navigate the job market.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{--
                     <div class="ps-md-4 ps-xl-0">
                         <div class="steps steps-hoverable">
                             <div class="step py-2 py-xl-3">
                                 <div class="step-number">
                                     <div class="step-number-inner">M</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Motivation</h3>
-                                    <p class="mb-0 text-black-50">We create a positive and motivating environment to help our clients reach their full potential and exceed their own expectations.</p>
+                                    <p class="mb-0 text-black-50">
+                                        We create a positive and motivating environment to help our clients reach their full potential and exceed their own expectations.
+                                    </p>
                                 </div>
                             </div>
                             <div class="step py-2 py-xl-3">
                                 <div class="step-number">
                                     <div class="step-number-inner">E</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Excellence</h3>
-                                    <p class="mb-0 text-black-50">We strive for the highest level of quality and performance in all aspects of our work, constantly seeking to improve and set high standards for ourselves and others. We commit to a process of continuous growth and development.</p>
+                                    <p class="mb-0 text-black-50">
+                                        We strive for the highest level of quality and performance in all aspects of our work, constantly seeking to improve and set high standards for ourselves and others. We commit to a process of continuous growth and development.
+                                    </p>
                                 </div>
                             </div>
                             <div class="step py-2 py-xl-3">
                                 <div class="step-number">
                                     <div class="step-number-inner">D</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Dedication</h3>
                                     <p class="mb-0 text-black-50">We are dedicated to providing a high-quality service and continuously improving our training and recruitment methods to meet the needs of our clients.</p>
                                 </div>
@@ -518,7 +591,7 @@
                                 <div class="step-number">
                                     <div class="step-number-inner">I</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Inclusivity</h3>
                                     <p class="mb-0 text-black-50">We foster an inclusive culture that embraces the British values of fairness and tolerance of all in the workplace.</p>
                                 </div>
@@ -527,7 +600,7 @@
                                 <div class="step-number">
                                     <div class="step-number-inner">C</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Community</h3>
                                     <p class="mb-0 text-black-50">We strive to build a strong community of medical professionals that can support each other in their professional development and career advancement.</p>
                                 </div>
@@ -536,13 +609,14 @@
                                 <div class="step-number">
                                     <div class="step-number-inner">S</div>
                                 </div>
-                                <div class="step-body">
+                                <div class="step-body bg-body p-3 rounded-3">
                                     <h3 class="h5 pb-1 mb-2">Support</h3>
                                     <p class="mb-0 text-black-50">We offer unparalleled support to our clients, both during their training and as they navigate the job market.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    --}}
                 </div>
             </div>
         </section>
