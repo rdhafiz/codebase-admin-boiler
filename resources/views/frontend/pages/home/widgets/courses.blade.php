217800a24ef8227d3cb854e7d3e8c6460d7deeff
Faozul Azim Rafi <rafi_ccj@hotmail.com>
2024-01-10 20:07:20 +0600

    modify design

--- resources/views/frontend/pages/home/widgets/courses.blade.php
+++ resources/views/frontend/pages/home/widgets/courses.blade.php
@@ -1,25 +1,25 @@
 
-<section id="section-courses" class="bg-secondary">
+<section id="section-courses" class="bg-secondary" style="background-color: rgb(16, 207, 201) !important;">
     <div class="container py-5">
 
         <div class="row pt-2">
 
             <div class="col-sm-12 col-md-4 pb-4 mb-1">
-                <div class="course shadow-lg border bg-body p-4 mx-sm-auto" data-aos="fade-right" data-aos-delay="500">
+                <div class="course shadow-lg border bg-body p-4 mx-sm-auto rounded-1" data-aos="fade-right" data-aos-delay="500">
                     <h3 class="h4 pt-3">15 Days (10 Stations)</h3>
                     <p class="mb-3">£ 1,200 fees, 24/7 access from anywhere, blended and instructor-led learning, completed within 15 days</p>
                     <a class="btn btn-sm btn-theme" href="{{route('front.apply')}}"><i class="ai-calendar-minus pe-2"></i> Apply Now</a>
                 </div>
             </div>
             <div class="col-sm-12 col-md-4 pb-4 mb-1">
-                <div class="course  shadow-lg border bg-body p-4 mx-sm-auto" data-aos="zoom-in" data-aos-delay="500">
+                <div class="course  shadow-lg border bg-body p-4 mx-sm-auto rounded-1" data-aos="zoom-in" data-aos-delay="500">
                     <h3 class="h4 pt-3">7 Days (10 Stations)</h3>
                     <p class="mb-3">£ 1,200 fees, 24/7 access from anywhere, blended and instructor-led learning, completed within 15 days</p>
                     <a class="btn btn-sm btn-theme" href="{{route('front.apply')}}"><i class="ai-calendar-minus pe-2"></i> Apply Now</a>
                 </div>
             </div>
             <div class="col-sm-12 col-md-4 pb-4 mb-1">
-                <div class="course  shadow-lg border bg-body p-4 mx-sm-auto" data-aos="fade-left" data-aos-delay="500">
+                <div class="course  shadow-lg border bg-body p-4 mx-sm-auto rounded-1" data-aos="fade-left" data-aos-delay="500">
                     <h3 class="h4 pt-3">3 Days (10 Stations)</h3>
                     <p class="mb-3">£ 1,200 fees, 24/7 access from anywhere, blended and instructor-led learning, completed within 15 days</p>
                     <a class="btn btn-sm btn-theme" href="{{route('front.apply')}}"><i class="ai-calendar-minus pe-2"></i> Apply Now</a>
