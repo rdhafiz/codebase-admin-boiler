055cf75b3105cf1b2c1180b35b6afbebf4555a2e
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-01-11 20:43:58 +0600

    why choose us

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -21,10 +21,11 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js" integrity="sha512-rCjfoab9CVKOH/w/T6GbBxnAH5Azhy4+q1EXW5XEURefHbIkRbQ++ZR+GBClo3/d3q583X/gO4FKmOFuhkKrdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     {{--    <script src="{{ asset('assets/js/vendor/jarallax.min.js') }}"></script>--}}
     <script>
         $(function (){
-            $('.owl-carousel').owlCarousel({
+            $('#home-banner-slide').owlCarousel({
                 loop:true,
                 margin:0,
                 nav:false,
@@ -34,6 +35,17 @@
                 autoplayTimeout:5000,
                 autoplayHoverPause:false,
                 animateOut: 'fadeOut'
+            })
+            window.whyChooseSlide = $('#why-choose-slide').owlCarousel({
+                loop:false,
+                margin:0,
+                nav:true,
+                dots:false,
+                items:2,
+                autoplay:false,
+                stagePadding: 100,
+                navText : ['',''],
+                smartSpeed:1000,
             })
             AOS.init();
         })
