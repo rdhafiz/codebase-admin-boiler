0316826c4f0f3643fb033f9062a90a15e392379a
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-01-17 19:22:28 +0600

    todays feedback

--- resources/views/frontend/pages/home/widgets/why-choose-us.blade.php
+++ resources/views/frontend/pages/home/widgets/why-choose-us.blade.php
@@ -99,58 +99,62 @@
 </div>
 <div class="why-choose-us extra-pad" style="background-image: url({{ asset('assets/images/frontend/home/about_us.jpg') }})">
 
-    <div class="w-100 d-none d-xl-flex align-content-end justify-content-center">
-        <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000" style="background-image: url({{asset('assets/images/shapes/choose-1.svg')}})">
-            <ul class="cube_rains">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div class="w-100 px-3">
-                <h2><strong>Practical Based</strong></h2>
-                <p class="m-0 text-dark">
-                    We'll give you lots of materials to use before you come to our training
-                    sessions, both online and in person. This includes videos, slideshows,
-                    and forms to help you practice. You'll also get to try some mock exams
-                    to get a feel for what it's really like, and you'll get feedback from
-                    our teachers to help you improve. Plus, we'll give you a study guide to
-                    help you learn on your own.
-                </p>
+    <div class="w-100 d-none d-xl-flex align-content-end justify-content-between">
+        <div class="d-flex justify-content-start">
+            <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000" style="background-image: url({{asset('assets/images/shapes/choose-1.svg')}})">
+                <ul class="cube_rains">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <div class="w-100 px-3">
+                    <h2><strong>Practical Based</strong></h2>
+                    <p class="m-0 text-dark">
+                        We'll give you lots of materials to use before you come to our training
+                        sessions, both online and in person. This includes videos, slideshows,
+                        and forms to help you practice. You'll also get to try some mock exams
+                        to get a feel for what it's really like, and you'll get feedback from
+                        our teachers to help you improve. Plus, we'll give you a study guide to
+                        help you learn on your own.
+                    </p>
+                </div>
+            </div>
+            <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="1000" style="background-image: url({{asset('assets/images/shapes/choose-2.svg')}})">
+                <ul class="cube_rains">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <div class="w-100 px-3">
+                    <h2 class="text-white">Online Platform</h2>
+                    <p class="m-0 text-white">
+                        Even after our training sessions are over, you'll still have access to
+                        all our resources and guidelines through our online learning platform.
+                        This way, you can keep learning and practicing until you take your exam.
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="1000" style="background-image: url({{asset('assets/images/shapes/choose-2.svg')}})">
-            <ul class="cube_rains">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div class="w-100 px-3">
-                <h2 class="text-white">Online Platform</h2>
-                <p class="m-0 text-white">
-                    Even after our training sessions are over, you'll still have access to
-                    all our resources and guidelines through our online learning platform.
-                    This way, you can keep learning and practicing until you take your exam.
-                </p>
-            </div>
-        </div>
-        <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000" style="background-image: url({{asset('assets/images/shapes/choose-3.svg')}})">
-            <ul class="cube_rains">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div class="w-100 px-3">
-                <h2>Exam Support</h2>
-                <p class="m-0 text-dark">
-                    We don't just stop once the training is over. We'll give you support
-                    before your exam with a mentor, our online learning platform, and a
-                    study guide with a suggested timeline. We'll also have competence tests
-                    and resit preparation to make sure you're ready to do your best on exam
-                    day.
-                </p>
+        <div class="">
+            <div class="each-choose-box" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000" style="background-image: url({{asset('assets/images/shapes/choose-3.svg')}})">
+                <ul class="cube_rains">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <div class="w-100 px-3">
+                    <h2>Exam Support</h2>
+                    <p class="m-0 text-dark">
+                        We don't just stop once the training is over. We'll give you support
+                        before your exam with a mentor, our online learning platform, and a
+                        study guide with a suggested timeline. We'll also have competence tests
+                        and resit preparation to make sure you're ready to do your best on exam
+                        day.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
