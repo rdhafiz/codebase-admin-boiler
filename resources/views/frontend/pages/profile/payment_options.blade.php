3f849fcae8117555dbd550b9fc8740b403aff7bb
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2024-01-18 19:35:03 +0600

    new pages

---
+++ resources/views/frontend/pages/profile/payment_options.blade.php
@@ -0,0 +1,58 @@
+@extends('frontend.layout.layout')
+
+@section('stylesheet')
+@show
+
+@section('content')
+
+    <div class="w-100 bg-secondary py-5 mt-5">
+        <div class="w-100 py-5">
+            <div class="container-lg">
+                <div class="row">
+
+                    <!-- Page content -->
+                    <div class="col-lg-8 offset-lg-2 pt-4 pb-2 pb-sm-4">
+                        <h1 class="h2 mb-4">Payment Options</h1>
+
+                        <div class="w-100">
+                            @if($errors->has('success'))
+                                <p class="alert alert-success">{{$errors->first('success')}}</p>
+                            @endif
+                            @if($errors->has('error'))
+                                <p class="alert alert-danger">{{$errors->first('error')}}</p>
+                            @endif
+                        </div>
+
+                        <section class="card border-0 py-1 p-md-2 p-xl-3 p-xxl-4">
+                            <div class="card-body">
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <a href="{{$card_payment_url}}"
+                                           class="w-100 d-inline-block card card-hover-primary text-decoration-none bg-secondary p-5 text-center rounded rounded-3 mb-3">
+                                            <i class="ai-card fs-1"></i>
+                                            <h3>Card Payment</h3>
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <a href="#"
+                                           class="w-100 d-inline-block card card-hover-primary text-decoration-none bg-secondary p-5 text-center rounded rounded-3">
+                                            <i class="ai-dollar fs-1"></i>
+                                            <h3>Bank Transfer</h3>
+                                        </a>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </section>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('js')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,7 @@ Route::group(['middleware' => [UserAuthCheck::class]], function () {
 Route::group(['middleware' => [UserAuthReq::class]], function () {
     Route::get('/logout', [AuthController::class, "logout"])->name('front.logout');
 
+    Route::get('/dashboard', [ProfileController::class, "dashboard"])->name('front.dashboard');
     Route::get('/profile', [ProfileController::class, "profile"])->name('front.profile');
     Route::get('/profile/update', [ProfileController::class, "profileUpdate"])->name('front.profile.update');
     Route::post('/profile/update', [ProfileController::class, "profileUpdateAction"])->name('front.profile.update.action');
